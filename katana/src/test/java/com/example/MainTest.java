10b917037716cf09c5524736dc1c4972cf074edf
Yuan Jian Puah <pyuanjian@google.com>
2023-09-15 10:40:43 +0800

    inital checkin

---
+++ katana/src/test/java/com/example/MainTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+
+    // @Disabled
+    @Test
+    @DisplayName("Main Test")
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
