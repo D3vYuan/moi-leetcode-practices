e4ef068896478ed5de64335ddc337c0430f96d71
TheMaos <yuanjian@hotmail.com>
2023-12-28 12:19:34 +0800

    added practice and solutions

--- src/main/java/com/example/solution/P94_Binary_Tree_Inorder_Traversal.java
+++ src/main/java/com/example/solution/P94_Binary_Tree_Inorder_Traversal.java
@@ -3,25 +3,6 @@ package com.example.solution;
 import java.util.ArrayList;
 import java.util.List;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class P94_Binary_Tree_Inorder_Traversal {
     /**
      * Reference:
